f59c7f1c520c24d8796998f5240204e169d64f77
Xuan liu <xuanliu@uw.edu>
2020-07-09 19:01:03 -0700

    updates

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component.jsx';
+import {Route, Switch} from 'react-router-dom';
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+)
 
 class App extends Component {
   render() {
-    return (<div > <HomePage/> </div>);
+    return (
+      <div >
+        <Switch>
+          <Route path='/' component={HomePage} />
+          <Route path='/hats' component={HatsPage}/>
+        </Switch>
+      </div>
+      
+      
+      );
   }
 }
 
